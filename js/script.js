5347aef328f89b1f592e9825b5b4d2ffe715c536
Klaudia Perszke <klaudia.perszke@gmail.com>
2025-07-25 14:15:58 +0200

    add geme score

--- js/script.js
+++ js/script.js
@@ -33,3 +33,40 @@ else if(playerInput == '3'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Ty przegrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+else if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ty przegrywasz!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Ty przegrywasz!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+  printMessage('Wpisz 1,2 lub 3!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+  printMessage('Wpisz 1,2 lub 3!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+  printMessage('Wpisz 1,2 lub 3!');
+}
